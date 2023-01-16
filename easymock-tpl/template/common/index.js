13257ff71dd8cbd3ba0edb7f2d31aa3e5047ecdc
GRIT <136907108@qq.com>
2023-01-16 11:33:03 +0800

    Delete easymock-tpl directory

--- easymock-tpl/template/common/index.js
+++
@@ -1,6 +0,0 @@
-<% _.forEach(config.projects, function(project){ %>import  {{$$.convertUrl(project.name)}} from './{{project.name}}/index';
-<% }) %>
-
-export default {<% _.forEach(config.projects, function(project, i){ %>
-  {{$$.convertUrl(project.name)}}<% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
-};
